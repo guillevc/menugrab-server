4d813156bb2babe614ac3d50cf5c62ebf1a7e83a
Guillermo Alfonso Varela Chouciño <guillevch@gmail.com>
2021-03-14 01:50:00 +0100

    fix

--- src/routes/api/users/index.js
+++ src/routes/api/users/index.js
@@ -5,7 +5,7 @@ const routes = async (app, options) => {
   app.get('/:userId/orders', { schema: getUserOrdersSchema, preValidation: [app.requireFirebaseAuth] }, async (req, reply) => {
     const { userId } = req.params;
 
-    if (userId !== app.user?.uid) {
+    if (userId !== req.user?.uid) {
       throw app.httpErrors.forbidden();
     }
 
